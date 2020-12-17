ea09403b9fefcb31218b5c9585afd68e060a9a61
janhavi-borde <janhaviborde23@gmail.com>
2020-12-17 21:24:44 +0530

    send mail

---
+++ client/src/Form.js
@@ -0,0 +1,90 @@
+import React, { Component } from 'react'
+import axios from 'axios';
+class Form extends Component {
+    state={
+        name:'',
+        email:'',
+        message:'',
+        sent:false
+    }
+    handleName=(e)=>{
+        this.setState({
+            name:e.target.value
+        })
+    }
+    handleEmail=(e)=>{
+        this.setState({
+            email:e.target.value
+        })
+    }
+    handleMessage=(e)=>{
+        this.setState({
+            message:e.target.value
+        })
+    }
+    formSubmit=(e)=>{
+        e.preventDefault();
+        let data={
+            name:this.state.name,
+            email:this.state.email,
+            message:this.state.message
+        }
+        axios.post('/api/forma',data)
+        .then(res=>{
+            this.setState({
+                sent:true,
+            },this.resetForm())
+        })
+        .catch(()=>{
+            console.log('message not send ')
+        })
+
+    }
+    resetForm=()=>{
+        this.setState({
+            name:'',
+            email:'',
+            message:''
+        })
+        setTimeout(() => {
+            this.setState({
+                sent:false,
+            })
+        }, 3000);
+    }
+    render() { 
+        return ( 
+            <div className="container">
+                <form onSubmit={this.formSubmit}>
+                    <div className="form-group">
+                        <label htmlFor="name">name</label>
+                        <input type="text" 
+                        name="name" 
+                        placeholder="name....." 
+                        onChange={this.handleName} 
+                        value={this.state.name}
+                        ></input>
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="email">Email</label>
+                        <input type="text" name="email" placeholder="email....." onChange={this.handleEmail} value={this.state.email}
+                        ></input>
+                    </div>
+                    <div className="form-group textArea">
+                        <label htmlFor="message">message</label>
+                        <textarea type="text" name="message" placeholder="message..." onChange={this.handleMessage}
+                        rows="5" cols="30"
+                         value={this.state.message}
+                         ></textarea>
+                    </div>
+                    <div className={this.state.sent?'msg msgAppear':'msg'}>
+                        message has been sent
+                    </div>
+                    <button className="btn" type="submit">submit</button>
+                </form>
+            </div>
+         );
+    }
+}
+ 
+export default Form;
---
+++ index.js
@@ -0,0 +1,48 @@
+const nodemailer = require('nodemailer');
+const bodyParser=require('body-parser');
+const express=require('express');
+const cors = require('cors');
+const app =express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(cors());
+
+app.post('/api/forma',(req,res)=>{
+    let data=req.body
+    let smtpTransporter = nodemailer.createTransport({
+        service: 'Gmail',
+        port:465,
+        auth: {
+            user: 'janhaviborde23@gmail.com',
+            pass: 'avinashharsha'
+        }
+    });
+let mailOptions = {
+    from: 'janhaviborde23@gmail.com',
+    to: 'janhaviborde23@gmail.com',
+    subject: `message from ${data.name}`,
+    html:`
+    <h3>imformaions</h3>
+    <ul>
+        <li>Name : ${data.name}</li>
+        <li>email : ${data.email}</li>
+    </ul>
+    <h3>Message</h3>
+    <p>${data.message}</p>
+    `
+};
+
+smtpTransporter.sendMail(mailOptions,(error,response)=>{
+    if(error){
+        res.send(error)
+    }else{
+        res.send('Success')
+    }
+})
+smtpTransporter.close()
+ 
+})
+const PORT =process.env.PORT||3001;
+app.listen(PORT,()=>{
+    console.log(`server starting at port ${PORT}`)
+})
